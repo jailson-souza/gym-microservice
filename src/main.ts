a150752bf00b39dc891cadf41fdec1d653bed4b4
Jailson de Souza <jailson.souza@compasso.com.br>
2023-10-30 20:25:13 -0300

    change dest

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,20 @@
-import { NestFactory } from '@nestjs/core';
+// import { NestFactory } from '@nestjs/core';
+// import AppsModule from './apps/apps.module';
+// import { ValidationPipe } from '@nestjs/common';
+
 import AppsModule from './apps/apps.module';
-import { ValidationPipe } from '@nestjs/common';
+import { bootstrap } from './shared/utils/tools/bootstrap';
+import { expressFunction } from './shared/utils/tools/express-function';
 
-async function bootstrap() {
-  const PORT = process.env.PORT;
-  const app = await NestFactory.create(AppsModule);
-  app.useLogger(false);
-  app.enableCors();
-  app.useGlobalPipes(new ValidationPipe());
-  app.listen(PORT, () => console.log(`server is running in port ${PORT}`));
-}
+// async function bootstrap() {
+//   const PORT = process.env.PORT;
+//   const app = await NestFactory.create(AppsModule);
+//   app.useLogger(false);
+//   app.enableCors();
+//   app.useGlobalPipes(new ValidationPipe());
+//   app.listen(PORT, () => console.log(`server is running in port ${PORT}`));
+// }
 
-bootstrap();
+// bootstrap();
+
+export default expressFunction(bootstrap(AppsModule));
