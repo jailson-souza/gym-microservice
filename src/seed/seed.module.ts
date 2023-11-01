25c5aced09923d9ba36419a1ddb15b91050df78e
Jailson de Souza <jailson.souza@compasso.com.br>
2023-11-01 16:48:01 -0300

    add seed

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -3,9 +3,16 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { PrismaService } from 'src/shared/utils/prisma';
 import { MuscleSeedService } from './muscle/muscle-seed.service';
 import { SeedService } from './seed.service';
+import { ExerciseSeedService } from './exercise/exercise-seed.service';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true })],
-  providers: [PrismaService, ConfigService, SeedService, MuscleSeedService],
+  providers: [
+    PrismaService,
+    ConfigService,
+    SeedService,
+    MuscleSeedService,
+    ExerciseSeedService,
+  ],
 })
 export class SeedModule {}
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { MuscleSeedService } from './muscle/muscle-seed.service';
+import { ExerciseSeedService } from './exercise/exercise-seed.service';
 
 @Injectable()
 export class SeedService {
-  constructor(private readonly muscleSeedService: MuscleSeedService) {}
+  constructor(
+    private readonly muscleSeedService: MuscleSeedService,
+    private readonly exerciseSeedService: ExerciseSeedService,
+  ) {}
 
   async run() {
     await this.muscleSeedService.run();
+    await this.exerciseSeedService.run();
   }
 }
