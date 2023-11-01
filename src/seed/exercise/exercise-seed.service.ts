cf35c53413a34c1ea03ee9eff5b89ca128f507b4
Hewerton Adao da Paz <hewerton.paz@compasso.com.br>
2023-11-01 17:33:56 -0300

    fiz data.json json keys

--- src/seed/exercise/exercise-seed.service.ts
+++ src/seed/exercise/exercise-seed.service.ts
@@ -7,16 +7,14 @@ export class ExerciseSeedService {
   constructor(private readonly prisma: PrismaService) {}
 
   async run() {
-    const exercisesMappeds = data.reduce(
-      (acc, { exercises }) => [...acc, ...exercises],
-      [],
-    );
+    const exercisesMappeds = data.reduce((acc, { exercises }) => {
+      return exercises?.length > 0 ? [...acc, ...exercises] : [...acc];
+    }, []);
 
     const exercises = await this.prisma.exercise.findMany({
       where: { id: { in: exercisesMappeds.map(({ id }) => id) } },
       select: { id: true, name: true },
     });
-
     const newExercises = exercisesMappeds
       .filter(({ id }) => !exercises.some((m) => m.id === id))
       ?.map(({ id, name, muscleId, image }) => ({
@@ -25,7 +23,6 @@ export class ExerciseSeedService {
         muscleId,
         image: `https://raw.githubusercontent.com/jailson-souza/gym-microservice/main/images/${image}`,
       }));
-
     if (newExercises?.length > 0) {
       await this.prisma.exercise.createMany({ data: newExercises });
     }
